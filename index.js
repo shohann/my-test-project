169e7793b723b03cf0fa8526daff3cebdffad151
Shohanur Rahman <shohanurr490@gmail.com>
2024-08-20 17:50:34 +0600

    add logger in config

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 // Import required modules
-const express = require("express");
+// const express = require("express");
 const dotenv = require("dotenv");
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Create an Express application
-const app = express();
+// const app = express();
 const port = process.env.PORT || 3000;
 
-// Health check route
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "UP" });
-});
+console.log(port);
+console.log("TEST TEST TEST");
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// // Health check route
+// app.get("/health", (req, res) => {
+//   res.status(200).json({ status: "UP" });
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
